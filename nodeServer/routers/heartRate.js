5f0298c6577706fdfcb57ba0ca78146ef2a28ccd
duychan <ohmybest02@gmail.com>
2022-04-07 21:58:17 +0700

    dot

--- nodeServer/routers/heartRate.js
+++ nodeServer/routers/heartRate.js
@@ -1,45 +1,45 @@
-const express = require("express");
-const router = express.Router();
-const { Heart } = require("../models/heart");
-const { User } = require("../models/user");
-const client = require("../clientSK");
-const { urlencoded } = require("express");
+const express = require("express")
+const router = express.Router()
+const { Heart } = require("../models/heart")
+const { User } = require("../models/user")
+const client = require("../clientSK")
+const { urlencoded } = require("express")
 
 router
   .route("/")
   .post(async (req, res, next) => {
     try {
       if (req.session.user) {
-        const userId = req.session.user;
+        const userId = req.session.user
         //console.log(userId);
-        const date = new Date().getTime();
-        const { BPM, level } = req.body;
-        const newHeart = new Heart({ date, BPM, level });
-        const userFounded = await User.findOne({ userId });
+        const date = new Date().getTime()
+        const { BPM, level } = req.body
+        const newHeart = new Heart({ date, BPM, level })
+        const userFounded = await User.findOne({ userId })
         //console.error(userFounded);
-        newHeart.owner = userFounded;
-        await newHeart.save();
-        userFounded.hearts.push(newHeart);
-        arrHeart.push(newHeart);
-        await userFounded.save();
-        res.status(201).json(userFounded);
+        newHeart.owner = userFounded
+        await newHeart.save()
+        userFounded.hearts.push(newHeart)
+        arrHeart.push(newHeart)
+        await userFounded.save()
+        res.status(201).json(userFounded)
       } else {
-        console.log("no session");
+        console.log("no session")
       }
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
   })
   .get(async (req, res, next) => {
-    const userId = req.session.user;
-    const user = await User.findById(userId);
-    arrHeart = user.hearts;
+    const userId = req.session.user
+    const user = await User.findById(userId)
+    arrHeart = user.hearts
     const x = arrHeart.map(async (e) => {
-      return await Heart.findOne({ e });
-    });
+      return await Heart.findOne({ e })
+    })
     Promise.all(x).then((val) => {
-      res.status(201).json(val);
-    });
-  });
+      res.status(201).json(val)
+    })
+  })
 
-module.exports = router;
+module.exports = router
